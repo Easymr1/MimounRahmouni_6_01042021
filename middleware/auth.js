9a1285a59a529f5d34558a1582b507d2e10d3dcb
Mimoun Rahmouni <mimoun06270@hotmail.fr>
2021-05-07 15:54:51 -0400

    clé Token dans la variable d'environnement

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'Je_mets_ce_que_je_veux_pour_crypter');
+        const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
         const userId = decodedToken.userId;
         if (req.body.userId && req.body.userId !== userId) {
             throw 'User Id non valable';
